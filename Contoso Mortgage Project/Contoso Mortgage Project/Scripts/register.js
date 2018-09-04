46a4320e63b65183219713381838c6e2c9e94495
Jace Williams <jacewill963@hotmail.com>
2018-09-04 00:11:59 -0400

    Register Validation Commit

--- Contoso Mortgage Project/Contoso Mortgage Project/Scripts/register.js
+++ Contoso Mortgage Project/Contoso Mortgage Project/Scripts/register.js
@@ -5,18 +5,15 @@ var formFields = {
 
 function submitForm() {
     let fields = [
-        "FirstName", "LastName", "Email", "SSN", "BusinessPhone", "MobilePhone", "AddressLine1", "AddressLine2", "City", "State", "Country", "ZipCode"
+        "FirstName", "LastName", "City", "State", "Country", "Email", "AddressLine1", "AddressLine2", "MobilePhone", "BusinessPhone", "SSN", "ZipCode"
     ];
+    validateAllFields(fields);
     let clearedValidation = true;
     for (var i = 0; i < fields.length; i++) {
         if (formFields[fields[i]] === false) {
             clearedValidation = false;
         }
-        if (document.getElementById(fields[i]).value.length === 0) {
-            clearedValidation = false;
-            formFields[i] = false;
-            document.getElementById(fields[i] + "Validate").innerHTML = "This field is required.";
-        }
+
     }
     if (clearedValidation) {
         //Ajax call
@@ -27,45 +24,81 @@ function submitForm() {
     }
 }
 //Form Validation=================================================
+function validateAllFields(fields) {
+    for (var i = 0; i < fields.length; i++) {
+        let element = document.getElementById(fields[i]);
+        if (i >= 0 && i < 5) {
+            validateChars(element);
+        } else if (i >= 5 && i < 8) {
+            validate(element);
+        } else if (i == 8 || i == 9) {
+            validatePhone(element);
+        } else if (i == 10) {
+            validateSSN(element);
+        } else {
+            validateNumbers(element);
+        }
+    }
+}
+
 
 //Validate All Charcters
 function validateCharsCheck(element) {
     var val = /^[a-zA-Z\s]+$/;
-    return val.test(element.value) ? true : false;
+    let results = val.test(element.value) ? true : false;
+    return results;
 }
 
 //Validate Symbols in a string
 function validateSymbolsCheck(element) {
     const val = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?\(\)_]/;
-    return val.test(element.value) ? true : false;
+    let results = val.test(element.value) ? true : false;
+    return results;
 }
 //Validate Numbers in a string
 function validateNumbersCheck(element) {
-    const numbers = /^\d*$/;
-    return (numbers.test(element.value)) ? true : false;
+    const val = /^\d*$/;
+    let results = val.test(element.value) ? true : false;
+    return results;
 }
 
 function validateSSNCheck(element) {
     const val = /^(?:\d{3}-\d{2}-\d{4}|\d{9})$/;
-    return val.test(element.value)? true : false;
+    let results = val.test(element.value) ? true : false;
+    return results;
 }
 
 function validatePhoneCheck(element) {
     const val = /^(?:\(\d{3}\)\d{3}-\d{4}|\d{3}-\d{3}-\d{4}|\d{10})$/;
-    var result = val.test(element.value);
-    return val.test(element.value) ? true : false;
+    let results = val.test(element.value) ? true : false;
+    return results;
+}
+
+function validate(element) {
+    let text = "";
+    if (element.value.length == 0) {
+        element.classList.add("is-invalid");
+        text = "This field is required.";
+        formFields[element.id] = false;
+    } else {
+        element.classList.remove("is-invalid");
+        formFields[element.id] = true;
+    }
+    document.getElementById(element.id + "Validate").innerHTML = text;
 }
 
 function validateChars(element) {
-    var results;
+    let results;
     let text = "";
-    if (validateCharsCheck(element) && !validateSymbolsCheck(element) || element.value.length === 0) {
+    if (validateCharsCheck(element) && !validateSymbolsCheck(element)) {
         text = "";
         element.classList.remove("is-invalid");
         results = true;
     } else {
         element.classList.add("is-invalid");
-        text = "Numeric values and symbols are not allowed in this field.";
+        text = (element.value.length !== 0) ?
+            "Numeric values and symbols are not allowed in this field." :
+            "This field is required.";
         results = false;
     }
     document.getElementById(element.id + "Validate").innerHTML = text;
@@ -73,32 +106,35 @@ function validateChars(element) {
 }
 
 function validateNumbers(element) {
-    var results;
-    console.log(validateNumbersCheck(element));
-    console.log(validateSymbolsCheck(element));
-    if ((validateNumbersCheck(element) && !validateSymbolsCheck(element)) || element.value.length === 0) {
-        text = "";
-        element.classList.remove("is-invalid");
-        results = (element.value.length !== 0) ? true : false;
-    } else {
-        element.classList.add("is-invalid");
-        text = "Only numeric values are allowed in this field.";
-        results = false;
-    }
-    document.getElementById(element.id + "Validate").innerHTML = text;
-    return results;
-}
-
-function validateSSN(element) {
-    var results;
+    let results;
     let text = "";
-    if (validateSSNCheck(element) && !validateCharsCheck(element) || element.value.length === 0) {
+    if ((validateNumbersCheck(element) && !validateSymbolsCheck(element)) && element.value.length !== 0) {
         text = "";
         element.classList.remove("is-invalid");
         results = true;
     } else {
         element.classList.add("is-invalid");
-        text = "Please input your 9 digit SSN, '-' are optional.";
+        text = (element.value.length !== 0) ?
+            "Only numeric values are allowed in this field." :
+            "This field is required.";
+        results = false;
+    }
+    document.getElementById(element.id + "Validate").innerHTML = text;
+    formFields[element.id] = results;
+}
+
+function validateSSN(element) {
+    let results;
+    let text = "";
+    if (validateSSNCheck(element) && !validateCharsCheck(element)) {
+        text = "";
+        element.classList.remove("is-invalid");
+        results = true
+    } else {
+        element.classList.add("is-invalid");
+        text = (element.value.length !== 0) ?
+            "Please input your 9 digit SSN, '-' are optional." :
+            "This field is required.";
         results = false;
     }
     document.getElementById(element.id + "Validate").innerHTML = text;
@@ -107,13 +143,15 @@ function validateSSN(element) {
 function validatePhone(element) {
     var results;
     let text = "";
-    if (validatePhoneCheck(element) && !validateCharsCheck(element) || element.value.length === 0) {
+    if (validatePhoneCheck(element) && !validateCharsCheck(element)) {
         text = "";
         element.classList.remove("is-invalid");
-        results = true;
+        results = (element.value.length !== 0) ? true : false;
     } else {
         element.classList.add("is-invalid");
-        text = "Please input your phone number digit SSN '()' and '-' are optional.";
+        text = (element.value.length !== 0) ?
+            "Please input your phone number digit SSN '()' and '-' are optional." :
+            "This field is required.";
         results = false;
     }
     document.getElementById(element.id + "Validate").innerHTML = text;
